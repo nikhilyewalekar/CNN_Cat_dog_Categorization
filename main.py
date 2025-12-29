c1e6838d054c7986986a976cd9c9a95ab3b5e376
nikhilyewalekar <nikhilyewalekar@gmail.com>
2025-12-29 21:37:16 +0530

    added file extension settings

--- main.py
+++ main.py
@@ -12,9 +12,21 @@ os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 def index():
     return render_template("index.html")
 
+ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg"}
+
+def allowed_file(filename):
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+
 @app.route("/uploadImage", methods=["POST"])
 def uploadImage():
+    if 'image_file' not in request.files:
+        return "No file", 400
+    
     file = request.files['image_file']
+
+    if not allowed_file(file.filename):
+        return "Invalid file type", 400
+
     file_name = os.path.join(UPLOAD_FOLDER, file.filename)
     file.save(file_name)
     obj = CNNCatDogClassification()
@@ -25,4 +37,5 @@ def uploadImage():
     return f"{result}"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    app.run(debug=False)
+    #app.run(host="0.0.0.0", port=8000)
